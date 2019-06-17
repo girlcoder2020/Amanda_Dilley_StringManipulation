8ef0cf5448dbba953f25b5b7551f548865f862d1
Amanda <48845910+girlcoder2020@users.noreply.github.com>
2019-06-16 22:24:56 -0500

    added java file
    used Eclipse to create this java file, and it helped immensely

---
+++ StringManipulation.java
@@ -0,0 +1,53 @@
+
+public class StringManipulation {
+
+//	public static void main(String[] args) {
+//		String str1 = "Hello ";
+//		String str2 = "World";
+//		str1 = str1.concat(str2);
+//		System.out.println(str1);
+//		
+//	}
+
+//	public static void main(String[] args) {
+//		String trimAndConcat = new String("  Hello World  ");
+//		trimAndConcat = trimAndConcat.trim();
+//		System.out.println(trimAndConcat.trim());
+//		
+//	}
+	
+//	public static void main(String[] args) {
+//		String manipulator = "Coding";
+//		int a = manipulator.indexOf('o');
+//		System.out.println(a); // 1
+		
+//		String manipulator = "Hello World";
+//		int b = manipulator.indexOf('o');
+//		System.out.println(b); // 4
+		
+//		String manipulator = "Hi";
+//		int c = manipulator.indexOf('o');
+//		System.out.println(c); // null
+		
+//	}
+//	public static void main(String[] args) {
+//		String DexNull = "Hello";
+//		String word = "Hello";
+//		String subString = "llo";
+//		String notSubString = "world";
+//		
+//		int a = DexNull.indexOf(subString);
+//		System.out.println(a); // 2
+//		
+//		int b = DexNull.indexOf(notSubString);
+//		System.out.println(b); // null
+//	}
+	public static void main(String[] args) {
+		String word = "Hello";
+		String subString = "world";
+		
+		System.out.println(word.charAt(1) + subString);
+	}
+	
+	
+}
